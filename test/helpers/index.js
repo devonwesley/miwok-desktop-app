c49b4bd1b02d631197c257803b21fe6d59e4956a
Devon Wesley <dev@blockchaindevelopers.io>
2018-12-13 14:57:21 -0800

    WIP: eslint working and setup. Just getting rid of some linting errors

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -2,14 +2,15 @@ import chai from 'chai';
 import jest from 'jest';
 
 function describeTest(message, callback) {
-  describe(message, function() {
-    callback();
+  /** eslint no-undef: "error" */
+  describe(message, function () { /** eslint-disable-line no-undef prefer-arrow-callback */
+    return callback();
   });
 }
 
 function itShould(message, callback) {
-  it(message, function() {
-    callback();
+  it(message, function () { /** eslint-disable-line no-undef prefer-arrow-callback */
+    return callback(); 
   });
 }
 
