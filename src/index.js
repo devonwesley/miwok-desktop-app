88daf9242e67716cca8ed9d27896638072c3c3f3
Devon Wesley <dev@blockchaindevelopers.io>
2018-12-19 16:50:46 -0800

    completed react setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
 // TODO: setup ENV vars to ignore this
 // line in production.
-require('electron-reload')(__dirname);
+if(process.env.NODE_ENV === 'development') {
+  require('electron-reload')(__dirname);
+}
 
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({ height: 600, width: 800 });
-  mainWindow.loadFile(`file://${__dirname}/app/index.html`);
+  if(process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.loadFile(path.join(__dirname, './app/index.html'));
 }
 
 app.on('ready', createWindow);
--- webpack.config.js
+++ webpack.config.js
@@ -47,4 +47,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.json', '.jsx'],
   },
+  node: {
+    __dirname: false,
+  },
 };
